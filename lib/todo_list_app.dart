48df1cc71053716e5a771268762e50af88c27ab3
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-06-13 23:13:32 +0200

    Etape 1 finie, tourne sur un Pixel 3a émulé, et sur chrome

---
+++ lib/todo_list_app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(const TodoListApp());
+}
+
+class TodoListApp extends StatefulWidget{
+  const TodoListApp({super.key});
+
+  @override
+  _TodoListAppState createState() => _TodoListAppState();
+}
+
+class _TodoListAppState extends State<TodoListApp>{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todo List'),
+        ),
+        body: const Center(
+          child: Text('Pour le TP Flutter'),
+        )
+      )
+    );
+  }
+
+}
